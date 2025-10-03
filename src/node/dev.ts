d6ddf1d4f887edb3d699294c5ab00b26e5cf90e1
zhangyedi03 <zhangyedi03@meituan.com>
2025-10-03 15:44:45 +0800

    feat: init project structure 🔥🔥

---
+++ src/node/dev.ts
@@ -0,0 +1,13 @@
+import { createServer } from "vite";
+
+/**
+ * @description Dev Server
+ * @param root Root path
+ * @returns Server
+ * **/
+export async function createDevServer(root = process.cwd()){
+  const server = await createServer({
+    root,
+  });
+  return server;
+}
